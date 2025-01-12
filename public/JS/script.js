defacec17699a474c5123719e4b8e38fc02541ba
Ankit Bhagat <ankitbhagat2062@gmail.com>
2025-01-12 22:30:15 +0545

    vbn

--- public/JS/script.js
+++ public/JS/script.js
@@ -210,7 +210,7 @@ async function mergeSelectedPages() {
     console.log("FormData before sending:", formData);
     
     // Make the fetch request to the Vercel API route
-    const response = await fetch('/merge.js', {
+    const response = await fetch('/api/merge.js', {
       method: 'POST',
       body: formData,
     });
