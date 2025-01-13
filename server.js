c5b6a89f97013300507b66fb050a7ffbf9fcbdcc
Ankit Bhagat <ankitbhagat2062@gmail.com>
2025-01-13 10:48:14 +0545

    cvbn

--- server.js
+++ server.js
@@ -2,9 +2,13 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+// Create an API endpoint
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
+
+// Export the app as a Vercel-compatible handler
+module.exports = app;
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
