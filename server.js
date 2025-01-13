97453fc188444ed9344f18a5f74ac592d0696a18
Ankit Bhagat <ankitbhagat2062@gmail.com>
2025-01-13 21:47:29 +0545

    cvbn m

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
-
+import { mergePdfs } from "./merge.js";
 // __dirname workaround for ES modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
