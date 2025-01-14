e5f0a7149c07c26256c366c557c28052cddc7a05
Ankit Bhagat <ankitbhagat2062@gmail.com>
2025-01-14 09:29:17 +0545

    dxcfvgbhn

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
