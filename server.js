2695e1edaccbf88a3e7b701fa82fc259ae2ffe90
Ankit Bhagat <ankitbhagat2062@gmail.com>
2025-01-14 09:28:02 +0545

    fcvgbhnjmk,

--- server.js
+++ server.js
@@ -3,9 +3,8 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 })
-
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
