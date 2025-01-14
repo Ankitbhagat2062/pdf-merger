d713e497cc4c323809e04046884cbfc17fd2da7a
Ankit Bhagat <ankitbhagat2062@gmail.com>
2025-01-14 09:54:26 +0545

    vbnm

--- server.js
+++ server.js
@@ -12,28 +12,27 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
-app.post('/merge', upload.array('pdfs', 12), function (req, res, next) {
-  const selectedPages = JSON.parse(req.body.selectedPages || "[]");
-   // Map file names (from the frontend) to actual uploaded file paths
-   const uploadedFiles = req.files.reduce((acc, file) => {
-    acc[file.originalname] = file.path; // Map original file name to its temp path
-    return acc;
-  }, {});
-
-  // Prepare ordered files with selected page ranges
-  const orderedFiles = selectedPages.map(({ fileName, pageNum }) => {
-    const uploadedFilePath = uploadedFiles[fileName];
-    if (!uploadedFilePath) {
-      throw new Error(`Uploaded file '${fileName}' not found.`);
-    }
-    return { path: uploadedFilePath, range: pageNum }; // Assuming you want to merge specific page ranges
-  });
-  mergedFileName = `_${Date.now()}.pdf`;
-  const outputFilePath = path.join(__dirname, "public", "merged_files", mergedFileName);
-    //   // Merge the selected pages from ordered files
-    await mergePdfs(orderedFiles, outputFilePath);
-     res.redirect(`/merged_files/${mergedFileName}`);
-})
+// app.post('/merge', upload.array('pdfs', 12), function (req, res, next) {
+//   const selectedPages = JSON.parse(req.body.selectedPages || "[]");
+//    // Map file names (from the frontend) to actual uploaded file paths
+//    const uploadedFiles = req.files.reduce((acc, file) => {
+//     acc[file.originalname] = file.path; // Map original file name to its temp path
+//     return acc;
+//   }, {});
+//   // Prepare ordered files with selected page ranges
+//   const orderedFiles = selectedPages.map(({ fileName, pageNum }) => {
+//     const uploadedFilePath = uploadedFiles[fileName];
+//     if (!uploadedFilePath) {
+//       throw new Error(`Uploaded file '${fileName}' not found.`);
+//     }
+//     return { path: uploadedFilePath, range: pageNum }; // Assuming you want to merge specific page ranges
+//   });
+//   mergedFileName = `_${Date.now()}.pdf`;
+//   const outputFilePath = path.join(__dirname, "public", "merged_files", mergedFileName);
+//     //   // Merge the selected pages from ordered files
+//     await mergePdfs(orderedFiles, outputFilePath);
+//      res.redirect(`/merged_files/${mergedFileName}`);
+// })
 
 app.listen(port, () => {
   console.log(`App is running on port ${port}`);
