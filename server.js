0e6cae25aeb60a26d8ceb7e5302383fd9432f909
Ankit Bhagat <ankitbhagat2062@gmail.com>
2025-01-14 09:31:55 +0545

    cvbnm

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3000;  // Vercel sets the port dynamically
+
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Route to serve the index.html
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
-})
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-// export default app;  // Vercel will use this export
+});
 
-  
+app.listen(port, () => {
+  console.log(`App is running on port ${port}`);
+});
